d1ea0606217f62a24883e228b7121682a77c267f
Annie  Lin <a.popcorn2@gmail.com>
2018-05-24 17:44:03 -0700

    java basics syntax

---
+++ Basics.java
@@ -0,0 +1,22 @@
+public class Basics{
+	public static void main(String[] args){
+
+		// Printing Basics
+
+		// Print with no new line
+		System.out.print("No new");
+		System.out.print(" line\n");
+
+		// Print WITH a new line
+		System.out.println("Yes new line");
+
+		
+		// Exercise 1: Print my Name
+		System.out.println("Annie");
+
+		// Excercise 2: Print Hello World
+		System.out.println("Hello World!");
+		System.out.println("(And all the people of the world)");
+
+	}
+}
